187de46d8936887daf111c2391820a940ba3aea2
prannoy <prannoy.mulmi@gmail.com>
2023-10-15 12:51:13 +0200

    Made docker container to communicate with http between server and client

--- smart-vehicle/main.py
+++ smart-vehicle/main.py
@@ -1,1 +1,18 @@
-print("Hello World - Client")
+# post_request.py
+
+import requests
+
+url = 'http://server:5000/data'  # Replace with the URL you want to POST to
+
+data = {
+    'GPS': '12',
+    'USER': 'test user'
+}
+response = requests.get('http://server:5000')
+print(response.text)
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(url, json=data, headers=headers)
+
+print(f"Response Status Code: {response.status_code}")
+print(f"Response Data: {response.text}")
