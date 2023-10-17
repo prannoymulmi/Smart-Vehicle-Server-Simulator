f64ec86942214adcb65f3d409a6cc1700d49d193
prannoy <prannoy.mulmi@gmail.com>
2023-10-17 18:03:50 +0200

    Updated socket code

--- client.py
+++ client.py
@@ -1,21 +1,17 @@
 import socket
 
-
-def capture_biometric_data():
-    # Simulate capturing biometric data
-    print("Please place your finger on the scanner...")
-    return "fingerprint_signature_123"  # In reality, this would be some unique data from the user
+SECRET = 'my_secret_password'
 
 
 def start_client():
     HOST = '127.0.0.1'
-    PORT = 65432
-
-    biometric_signature = capture_biometric_data()
+    PORT = 65431
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(biometric_signature.encode())
+
+        # Send the secret for authentication
+        s.sendall(SECRET.encode())
 
         response = s.recv(1024).decode()
         print(response)
--- server.py
+++ server.py
@@ -1,21 +1,32 @@
 import socket
 
-SECRET = 'my_secret_password'
+SECRET = 'my_secret_password'  # Both the client and server should have this
 
 
-def start_client():
+def start_server():
     HOST = '127.0.0.1'
-    PORT = 65432
+    PORT = 65431
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Server listening...")
 
-        # Send the secret for authentication
-        s.sendall(SECRET.encode())
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
 
-        response = s.recv(1024).decode()
-        print(response)
+            # Authentication
+            data = conn.recv(1024).decode()
+            if data == SECRET:
+                print(data)
+                conn.sendall(b'Authentication successful')
+                conn.close()
+                # Handle authenticated client
+            else:
+                conn.sendall(b'Authentication failed')
+                conn.close()
 
 
 if __name__ == "__main__":
-    start_client()
+    start_server()
