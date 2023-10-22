dbc7ed2e38151eee9446d482a93d9c912d7ac4d3
prannoy <prannoy.mulmi@gmail.com>
2023-10-22 10:18:02 +0200

    fixed client ssl issue

--- ssl/client_ssl.py
+++ ssl/client_ssl.py
@@ -10,7 +10,7 @@ def start_client():
 
     # Create a context for the secure connection
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    context.load_verify_locations('server_cert.pem')  # Load the server's certificate to verify it
+    context.load_verify_locations('ssl/server_cert.pem')  # Load the server's certificate to verify it
 
     # Wrap the socket in the SSL context
     with socket.create_connection((HOST, PORT)) as client_socket:
